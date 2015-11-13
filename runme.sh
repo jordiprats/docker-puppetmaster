33ebaa6ef5252374026da42e08b6eda0b2fca9ac
Jordi Prats <jprats@systemadmin.es>
2015-11-13 15:06:37 +0100

    eliminat if inutil

--- runme.sh
+++ runme.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-if [ ! -f /etc/puppet/puppet.conf ];
-then
 
-  touch /etc/puppet/hiera.yaml
+touch /etc/puppet/hiera.yaml
 
-  cat <<EOF > /tmp/manifest.pp
+cat <<EOF > /tmp/manifest.pp
 
-  class { 'puppet': }
+class { 'puppet': }
 
-	class { 'puppet::master':
-    certname => '${EYP_PUPPETFQDN}',
-    manage_service => false,
-	}
+class { 'puppet::master':
+  certname => '${EYP_PUPPETFQDN}',
+  manage_service => false,
+}
 
 EOF
 
-  puppet apply --modulepath=/usr/local/src/puppetmodules/ /tmp/manifest.pp
+puppet apply --modulepath=/usr/local/src/puppetmodules/ /tmp/manifest.pp
 
-fi
 
 /usr/sbin/apache2 -k start -D NO_DETACH
